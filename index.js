571bf11d7c16b1eb7f8c8d34824c310b7687c5f9
fisker <lionkay@gmail.com>
2023-07-07 18:01:53 +0800

    Meta files

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
-const {localStorage, CustomEvent} = window;
-const STORAGE_EVENT = 'storage';
+const {localStorage, CustomEvent} = window
+const STORAGE_EVENT = 'storage'
 
-const pick = (object, keys) => Object.fromEntries(keys.map(key => object[key]))
+const pick = (object, keys) =>
+  Object.fromEntries(keys.map((key) => object[key]))
 
 class CrossTabNotifier {
   constructor(key) {
-    const listeners = new Set();
+    const listeners = new Set()
 
     const storageEventListener = (event) => {
-      const eventData = event instanceof CustomEvent ? event.detail : event;
+      const eventData = event instanceof CustomEvent ? event.detail : event
 
-      const {
-        storageArea,
-        key
-      } = eventData
+      const {storageArea, key: eventDataKey} = eventData
 
-      if (storageArea !== localStorage || key !== this.key) {
-        return;
+      if (storageArea !== localStorage || eventDataKey !== key) {
+        return
       }
 
       const data = pick(eventData, ['oldValue', 'newValue', 'url', 'key'])
@@ -25,21 +23,27 @@ class CrossTabNotifier {
         message = JSON.parse(data.newValue)
       } catch {}
 
-      for (const listener of this.listeners) {
-        listener(message, data);
+      if (!message) {
+        return;
       }
-    };
 
-    this.key = key;
-    this.listeners = listeners;
+      for (const listener of listeners) {
+        try {
+          listener(message, data)
+        } catch {}
+      }
+    }
+
+    this.key = key
+    this.listeners = listeners
     this.storageEventListener = storageEventListener
-    window.addEventListener(STORAGE_EVENT, storageEventListener, false);
+    window.addEventListener(STORAGE_EVENT, storageEventListener, false)
   }
 
   notify(message) {
-    const {key} = this;
-    const oldValue = localStorage.getItem(key);
-    const newValue = JSON.stringify(message);
+    const {key} = this
+    const oldValue = localStorage.getItem(key)
+    const newValue = JSON.stringify(message)
 
     // Notify current tab self
     window.dispatchEvent(
@@ -49,25 +53,25 @@ class CrossTabNotifier {
           oldValue,
           storageArea: localStorage,
           key,
-          url: window.location.href
+          url: window.location.href,
         },
-      })
-    );
+      }),
+    )
 
-    localStorage.setItem(key, newValue);
+    localStorage.setItem(key, newValue)
   }
 
   listen(listener) {
-    this.listeners.add(listener);
+    this.listeners.add(listener)
   }
 
   stop(listener) {
-    this.listeners.delete(listener);
+    this.listeners.delete(listener)
   }
 
   destroy() {
-    window.removeEventListener(STORAGE_EVENT, this.storageEventListener, false);
+    window.removeEventListener(STORAGE_EVENT, this.storageEventListener, false)
   }
 }
 
-export default CrossTabNotifier;
+export default CrossTabNotifier
